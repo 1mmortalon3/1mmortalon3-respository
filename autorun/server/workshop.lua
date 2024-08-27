aa768c2cc40294f90b9509f848cc096877168aec
1mmortalon3 <addison.Johnston56@gmail.com>
2024-08-26 22:44:07 -0400

    Add files via upload

---
+++ autorun/server/workshop.lua
@@ -0,0 +1,91 @@
+resource.AddWorkshop( '852839002' ) -- Onyx Framework [Resources]
+resource.AddWorkshop( '906974084' ) -- Star wars Detpack
+resource.AddWorkshop( '727824870' ) -- [PR]Star Wars Medical Sweps [Bacta Grenade, Bacta Injector, Force Heal]
+resource.AddWorkshop( '2486994157' ) -- MSD UI Content only
+resource.AddWorkshop( '2689168958' ) -- MRS Content (Icons)
+resource.AddWorkshop( '2019568291' ) -- [TFA] StarWars Reworked Grenade Pack
+resource.AddWorkshop( '1741985166' ) -- [TFA] StarWars Reworked Shared Resources and Assets
+resource.AddWorkshop( '1705500509' ) -- [TFA] StarWars CIS Weapon Reworked Pack
+resource.AddWorkshop( '1720373368' ) -- [TFA] StarWars Galactic Weapon Reworked Pack
+resource.AddWorkshop( '1707874330' ) -- [TFA] StarWars Republic Weapon Reworked Pack
+resource.AddWorkshop( '889827473' ) -- [TFA Official] Star Wars Shared Resources [ Sounds, Icons, Shared Support Menu ]
+resource.AddWorkshop( '2938551981' ) -- TFA Base [Fixed Scopes]
+resource.AddWorkshop( '1595332211' ) -- GmodAdminSuite
+resource.AddWorkshop( '1452363997' ) -- mCompass
+resource.AddWorkshop( '2280867785' ) -- [AP] Star Wars - Sci-Fi Prop Pack
+resource.AddWorkshop( '284266415' ) -- Sci-fi Props Megapack
+resource.AddWorkshop( '3296143133' ) -- rp_abafar_ag_v1_2
+resource.AddWorkshop( '3248018078' ) -- Closetter - Content
+resource.AddWorkshop( '3238211247' ) -- [LVS] Space LAAT/i Gunship
+resource.AddWorkshop( '2607617652' ) -- [GG] STAR WARS The Clone Wars: Phase 2 91st Recon Corps (CGI)
+resource.AddWorkshop( '104482086' ) -- [Official] Precision Tool
+resource.AddWorkshop( '2513519717' ) -- Gmod Player Spawn Point Editor - Resources Only
+resource.AddWorkshop( '2916695744' ) -- SAM StarWarsRP Commands
+resource.AddWorkshop( '207948202' ) -- Simple ThirdPerson - Sliders & Fixes!
+resource.AddWorkshop( '757604550' ) -- [wOS] Animation Extension - Base (Full Version)
+resource.AddWorkshop( '2191184796' ) -- [CGI] HD Clone Cadet Pack
+resource.AddWorkshop( '666417939' ) -- Star Wars: Jedi Youngling Playermodels
+resource.AddWorkshop( '220336312' ) -- PermaProps
+resource.AddWorkshop( '133537219' ) -- Falco's Prop Protection (FPP)
+resource.AddWorkshop( '1801797040' ) -- [LN] CGI HD: 41st Elite Corps
+resource.AddWorkshop( '1461735659' ) -- Alydus's Fortification Builder Tablet
+resource.AddWorkshop( '493897275' ) -- Originahl Scripts - All Contents in one Pack
+resource.AddWorkshop( '2455732685' ) -- [RDV] Extra SAM Commands
+resource.AddWorkshop( '3083974478' ) -- KB GMS - Advanced Character Creator
+resource.AddWorkshop( '1907060869' ) -- Srlion's Hook Library
+resource.AddWorkshop( '1206775008' ) -- Star Wars - Imperial Materials
+resource.AddWorkshop( '1251088550' ) -- SW Modelpack : Venator
+resource.AddWorkshop( '946190478' ) -- Star Wars - Rogue One Prop Pack
+resource.AddWorkshop( '2370224653' ) -- [SRSP] Star Wars - Props Pack | v1.2
+resource.AddWorkshop( '2607559539' ) -- Aussie's Shared CGI Content
+resource.AddWorkshop( '2607569479' ) -- [GG] STAR WARS The Clone Wars: Phase 2 104th Wolfpack
+resource.AddWorkshop( '2102911039' ) -- Gargantuan SWTOR Prop Pack
+resource.AddWorkshop( '2115240153' ) -- [RDV] Intercom System
+resource.AddWorkshop( '1700003070' ) -- Star Wars Fusion Cutter
+resource.AddWorkshop( '2607553235' ) -- [GG] STAR WARS The Clone Wars: Phase 2 212th Attack Battalion (CGI)
+resource.AddWorkshop( '2608513883' ) -- [GG] STAR WARS The Clone Wars: Phase 2 Coruscant Guard (CGI)
+resource.AddWorkshop( '2609934737' ) -- [GG] STAR WARS The Clone Wars: Phase 2 Unassigned (CGI)
+resource.AddWorkshop( '2816230161' ) -- [NCS] Defcon System
+resource.AddWorkshop( '2912826012' ) -- [LVS] - Planes
+resource.AddWorkshop( '2919757295' ) -- [LVS] - Star Wars
+resource.AddWorkshop( '2919769293' ) -- [LVS] Star Wars Turrets
+resource.AddWorkshop( '2921140209' ) -- [LVS] Star Wars Clone Wars Pack
+resource.AddWorkshop( '2930121561' ) -- [LVS] Star Wars Misc Republic Fighters
+resource.AddWorkshop( '2935149491' ) -- STAR WARS The Clone Wars: CIS B1 & B2 Battle Droids
+resource.AddWorkshop( '2097133231' ) -- Jetpacks Pack
+resource.AddWorkshop( '3224079254' ) -- [LVS] - Star Wars | TCW / Misc | Automatic Capital Ship
+resource.AddWorkshop( '2525981436' ) -- CGI HD: Republic Navy and Military [Playermodels & Ragdolls]
+resource.AddWorkshop( '1912553584' ) -- Clone Wars Republic Base Prop Pack
+resource.AddWorkshop( '3239379835' ) -- [CGI] The Republic Navy | Lore Characters
+resource.AddWorkshop( '2577231984' ) -- [GG] STAR WARS The Clone Wars: Phase 2 501st Legion (CGI)
+resource.AddWorkshop( '108176967' ) -- The Sit Anywhere Script!
+resource.AddWorkshop( '2608400485' ) -- [GG] STAR WARS The Clone Wars: Phase 2 41st Elite Corps (CGI)
+resource.AddWorkshop( '2609924604' ) -- [GG] STAR WARS The Clone Wars: Alpha-Class ARC (CGI)
+resource.AddWorkshop( '2912816023' ) -- [LVS] - Framework
+resource.AddWorkshop( '2081830563' ) -- StarWars Ammo Dispensers
+resource.AddWorkshop( '3239326743' ) -- [CGI] The Republic Navy
+resource.AddWorkshop( '3015660769' ) -- [NCS] Shared Resources
+resource.AddWorkshop( '2696785946' ) -- [RDV] Library (v2)
+resource.AddWorkshop( '3207053863' ) -- [NCS] Defibrillator (Aeron Networks)
+resource.AddWorkshop( '1273525008' ) -- AT-TE
+resource.AddWorkshop( '882850372' ) -- Nu-Class Shuttle
+resource.AddWorkshop( '1916699070' ) -- Stryker's StarWars RP Information Panels
+resource.AddWorkshop( '109643223' ) -- 3D2D Textscreens
+resource.AddWorkshop( '2764507578' ) -- Star Wars: Textscreens
+resource.AddWorkshop( '1735982700' ) -- [CGI] Ultimate CIS NPC Pack
+resource.AddWorkshop( '1674752437' ) -- [TFA] StarWars Rocket Launcher Pack
+resource.AddWorkshop( '2441216112' ) -- Police Baton
+resource.AddWorkshop( '2441210098' ) -- Police Baton - Content
+resource.AddWorkshop( '740395760' ) -- Zhrom's Star Wars Prop Pack
+resource.AddWorkshop( '2217286711' ) -- [TFA][TW] Reciprocating Quad Blaster
+resource.AddWorkshop( '3304883408' ) -- Axel Gaming Defcon Sounds
+resource.AddWorkshop( '2943090068' ) -- Heart Turbolaser Tool
+resource.AddWorkshop( '2332782456' ) -- Star Wars Clone Wars Ships Prop Pack
+resource.AddWorkshop( '107905828' ) -- Simple Sound Browser
+resource.AddWorkshop( '1125980817' ) -- [wOS] Animation Extension - Riddick
+resource.AddWorkshop( '2917050373' ) -- [wOS] DynaBase Legacy Extensions
+resource.AddWorkshop( '2916561591' ) -- [wOS] DynaBase - Dynamic Animation Manager
+resource.AddWorkshop( '2646756503' ) -- [wOS] Animation Extension - Base  (Code Only)
+resource.AddWorkshop( '925184078' ) -- [wOS] Animation Extension - Experimental
+resource.AddWorkshop( '2840063133' ) -- [TFA] | Hud
+resource.AddWorkshop( '1881093005' ) -- aa [SL] Anti Adware
